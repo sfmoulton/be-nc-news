06989b5231c7c67a49eee6ca39f0ea1303dd5b79
Steph <stephfmoore@gmail.com>
2020-02-03 20:55:35 +0000

    finished seeding

--- db/data/development-data/index.js
+++ db/data/development-data/index.js
@@ -3,4 +3,3 @@ exports.commentData = require("./comments");
 exports.topicData = require("./topics");
 exports.userData = require("./users");
 
-//do I need to change this to the format of my other index.js file in the data folder?
