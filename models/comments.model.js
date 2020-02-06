baf9bdd7e28e29ac840ac9c6b3e77c517ed1939f
Steph <stephfmoore@gmail.com>
2020-02-06 15:46:50 +0000

    sorted out the issues with addArticleComment

--- models/comments.model.js
+++ models/comments.model.js
@@ -1,10 +1,11 @@
 const connection = require("../db/connection");
 
 exports.addArticleComment = (newComment, article_id) => {
+  newComment[0].article_id = article_id;
+  
   return connection("comments")
     .insert(newComment, "*")
     .then(comment => {
-      comment[0].article_id = article_id;
       return comment[0];
     });
 };
